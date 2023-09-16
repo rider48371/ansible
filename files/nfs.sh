aaed05aa6a8fb922d9822cdf1eabc8745ceb69fb
Fred <rider48371@gmail.com>
2023-09-16 19:40:29 -0400

    removing nfs

--- files/nfs.sh
+++
@@ -1,1 +0,0 @@
-sudo mount -t nfs 192.168.1.154:/nfs/Fred /home/fred/nas/fred & sudo mount -t nfs 192.168.1.154:/nfs/kari /home/fred/nas/kari & sudo mount -t nfs 192.168.1.154:/nfs/Public /home/fred/nas/public
