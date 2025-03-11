6063f047f54ddf1cf87aa616d972ce4c604030a8
Fred Rider <rider48371@gmail.com>
2025-03-11 02:04:34 -0400

    updating qtile and i3 for messenger

--- files/yay.sh
+++ files/yay.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-yay -S qtile-extras picom-git spotify rofi-bluetooth-git i3lock-color plex-desktop &
+yay -S qtile-extras picom-git spotify rofi-bluetooth-git i3lock-color plex-desktop messenger-nativefier &
 systemctl --user start clipmenud &
 systemctl --user enable clipmenud
