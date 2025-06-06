baf43e0b6947a3a06df6975043f5e95c333fe3f7
Fred <rider48371@gmail.com>
2025-06-05 20:31:27 -0400

    updating lazy function

--- files/qtile/config.py
+++ files/qtile/config.py
@@ -5,7 +5,7 @@ import socket
 import subprocess
 from libqtile import qtile
 from libqtile.config import Click, Drag, Group, KeyChord, Key, Match, Screen
-from libqtile.command import lazy
+from libqtile.lazy import lazy
 from libqtile import layout, bar, widget, hook
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
